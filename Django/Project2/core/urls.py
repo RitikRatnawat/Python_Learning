10760047d3ef2f5610f1f6b0bf09b22f95e2a957
RitikRatnawat <ritikratnawat786@gmail.com>
2024-02-17 18:23:28 +0530

    Learning about the Models and Migrations

---
+++ Django/Project2/core/urls.py
@@ -0,0 +1,10 @@
+"""Core App URLs"""
+
+from django.urls import path
+from core import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
+]
