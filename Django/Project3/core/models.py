f2db372aa15762f8ba14437283206e59018ad6f6
RitikRatnawat <ritikratnawat786@gmail.com>
2024-03-05 19:08:33 +0530

    Completed Basic CRUD operations using Django shell

--- Django/Project3/core/models.py
+++ Django/Project3/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    speed = models.IntegerField(default=60)
+
+    def __str__(self):
+        return self.name
