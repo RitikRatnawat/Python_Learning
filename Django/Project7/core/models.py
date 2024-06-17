cacd4e1b094ea5116084d0f6b4cc600b62ce92d2
RitikRatnawat <ritikratnawat786@gmail.com>
2024-06-18 03:07:40 +0530

    Done with Django signals

--- Django/Project7/core/models.py
+++ Django/Project7/core/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=100)
+    car_type = models.CharField(max_length=100)
+    speed = models.IntegerField(default=50)
+
+    def __str__(self) -> str:
+        return self.car_name
+
+
+@receiver(post_save, sender=Car)
+def get_speed(sender, instance, **kwargs):
+    print(f"Car Object {instance.car_name} Created")
+    print(sender)
+    print(kwargs)
+
+
+"""
+Types of Signals : 
+1. pre_save
+2. post_save
+3. pre_delete
+4. post_delete
+"""
--- Django/Project7/core/views.py
+++ Django/Project7/core/views.py
@@ -1,3 +1,13 @@
+import random
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Car
 
-# Create your views here.
+
+def cars(request):
+    car = Car.objects.create(car_name=f"Nexon-{random.randint(0, 100)}", car_type="SUV")
+    queryset = Car.objects.all()
+
+    json_data = {"data": [{"name": car.car_name, "type": car.car_type, "speed": car.speed} for car in queryset]}
+
+    return JsonResponse(json_data)
