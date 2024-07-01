19dcdefd89b1fc2774d427c105a8643346919698
RitikRatnawat <ritikratnawat786@gmail.com>
2024-07-02 03:25:24 +0530

    Implemented CRUD using Class based views

--- Django/Project8/core/urls.py
+++ Django/Project8/core/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from .views import EmployeeCreateView, EmployeeListView, EmployeeDetailView, EmployeeUpdateView
 
-url_patterns = [
-
+urlpatterns = [
+    path("create/", EmployeeCreateView.as_view(), name="employee_create"),
+    path("list/", EmployeeListView.as_view(), name="employee_list"),
+    path("detail/<int:pk>", EmployeeDetailView.as_view(), name="employee_detail"),
+    path("update/<int:pk>", EmployeeUpdateView.as_view(), name="employee_update"),
 ]
--- Django/Project8/core/views.py
+++ Django/Project8/core/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Employee
+from .forms import EmployeeForm
 
-# Create your views here.
+
+class EmployeeCreateView(CreateView):
+
+    model = Employee
+    fields = "__all__"
+
+    def get_success_url(self):
+        return reverse('employee_detail', kwargs={'pk': self.object.pk})
+
+
+class EmployeeListView(ListView):
+
+    model = Employee
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super(EmployeeListView, self).get_queryset(*args, **kwargs)
+        qs = qs.order_by("-last_name")
+        return qs
+
+
+class EmployeeDetailView(DetailView):
+
+    model = Employee
+
+
+class EmployeeUpdateView(UpdateView):
+
+    model = Employee
+    fields = "__all__"
+
+    def get_success_url(self):
+        return reverse('employee_detail', kwargs={'pk': self.object.pk})
