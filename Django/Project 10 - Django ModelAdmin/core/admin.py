867feba4992d0015601660c5c59317457e30c0b6
RitikRatnawat <ritikratnawat786@gmail.com>
2024-08-30 03:38:19 +0530

    Learned Basics of Django ModelAdmin and its filters

--- Django/Project 10 - Django ModelAdmin/core/admin.py
+++ Django/Project 10 - Django ModelAdmin/core/admin.py
@@ -1,3 +1,31 @@
+import string
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+# Documentation : https://docs.djangoproject.com/en/5.1/ref/contrib/admin/
+
+
+class NameListFilter(admin.SimpleListFilter):
+    title = "Name Starts With"
+
+    parameter_name = "filter_key"
+
+    def lookups(self, request, model_admin):
+        return [(ch, ch) for ch in list(string.ascii_uppercase)]
+
+    def queryset(self, request, queryset):
+
+        if self.value():
+            return queryset.filter(student_name__startswith=self.value().lower())
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ["id", "student_name", "student_email", "student_password"]
+    list_filter = [NameListFilter]
+
+
+admin.site.register(Student, StudentAdmin)
+
+
+
+
--- Django/Project 10 - Django ModelAdmin/core/models.py
+++ Django/Project 10 - Django ModelAdmin/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=50)
+    student_email = models.EmailField(max_length=50)
+    student_password = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.student_name
