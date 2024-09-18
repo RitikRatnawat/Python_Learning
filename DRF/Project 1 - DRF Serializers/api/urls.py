d6121a8709e560ff7de431949537e4521b05aab2
RitikRatnawat <ritikratnawat786@gmail.com>
2024-09-19 03:36:12 +0530

    Learned about the Basics of DRF Serializers

--- DRF/Project 1 - DRF Serializers/api/urls.py
+++ DRF/Project 1 - DRF Serializers/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import student_detail, all_student_details
 
 urlpatterns = [
-
+    path("stu-info/<int:pk>", student_detail, name="student_detail"),
+    path("all-stu-info/", all_student_details, name="all_student_details"),
 ]
--- DRF/Project 1 - DRF Serializers/api/views.py
+++ DRF/Project 1 - DRF Serializers/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse, JsonResponse
+from .models import Student
+from .serializers import StudentSerializer
 
-# Create your views here.
+
+def student_detail(request, pk):
+    student = Student.objects.get(id=pk)
+    st_serializer = StudentSerializer(student)
+
+    # json_data = JSONRenderer().render(st_serializer.data)
+
+    # return HttpResponse(json_data, content_type="application/json")
+    return JsonResponse(st_serializer.data)
+
+
+def all_student_details(request):
+    students = Student.objects.all()
+    sts_serializer = StudentSerializer(students, many=True)
+
+    # json_data = JSONRenderer().render(sts_serializer.data)
+
+    # return HttpResponse(json_data, content_type="application/json")
+    return JsonResponse(sts_serializer.data, safe=False)
+
