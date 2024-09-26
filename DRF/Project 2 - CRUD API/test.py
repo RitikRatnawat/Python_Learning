f4d39a63cd5daaa557d6db318ef9ad5e9e4ab345
RitikRatnawat <ritikratnawat786@gmail.com>
2024-09-27 03:45:12 +0530

    Added a test script to test endpoints

--- DRF/Project 2 - CRUD API/test.py
+++ DRF/Project 2 - CRUD API/test.py
@@ -1,20 +1,32 @@
-import json
+import random
 import requests
+from faker import Faker
+
+fake = Faker()
 
 
 url = 'http://localhost:8000/api/student'
 
 
 def get_student(id):
-    data = {}
-    if id is not None:
-        data["id"] = id
 
-    json_data = json.dumps(data)
-    response = requests.get(url, data=json_data)
+    response = requests.get(f"{url}/{id}")
+    print(response.status_code)
+    print(response.json())
+
+
+def add_student():
+    data = {
+        "name": fake.name(),
+        "roll_number": random.randint(106, 500),
+        "city": fake.city()
+    }
+
+    response = requests.post(url, data=data)
     print(response.status_code)
     print(response.json())
 
 
 if __name__ == '__main__':
-    get_student(1)
+    # get_student(6)
+    add_student()
